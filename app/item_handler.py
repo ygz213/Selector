e179e6131ece6194af952935af2e81927c8ecd3e
ygz213 <yigitgurbuz0630@outlook.com>
2024-07-09 12:03:44 +0300

    Added ability to refresh self.combo_box numbers

--- app/item_handler.py
+++ app/item_handler.py
@@ -6,3 +6,5 @@ def add_item(self):
     item, ok = QtWidgets.QInputDialog.getText(self, 'Add item', 'Item name:')
     if item and ok:
         self.list_widget.addItem(item)
+        self.combo_box.clear()
+        self.combo_box.addItems([str(item) for item in list(range(1, self.list_widget.count()))])
