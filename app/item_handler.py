100836653aee348b3bee6744ec0e808a910b6664
ygz213 <yigitgurbuz0630@outlook.com>
2024-07-16 15:11:48 +0300

    Cleaned up

--- app/item_handler.py
+++ app/item_handler.py
@@ -12,14 +12,14 @@ def select_item(self):
         self.list_widget.setSelectionMode(QtWidgets.QListWidget.SelectionMode.MultiSelection)
         for item in range(int(self.combo_box.currentText())):    # With this for loop, the program selects as many random items as user wants
             selected_item = choice(item_indices)    # Selects random item
-            selected_items.append(self.list_widget.item(selected_item).text())    # Adds random item to the "selected_items" list
-            self.list_widget.item(selected_item).setSelected(True)    # Shows random item in "self.list_widget"
-            item_indices.remove(selected_item)    # Deletes random item from "item_indices" so it can't be selected again
+            selected_items.append(self.list_widget.item(selected_item).text())    # Adds random item to the 'selected_items' list
+            self.list_widget.item(selected_item).setSelected(True)    # Shows random item in 'self.list_widget'
+            item_indices.remove(selected_item)    # Deletes random item from 'item_indices' so it can't be selected again
 
         QtWidgets.QMessageBox.information(self, 'Selected item(s)', f'''
                                           <ul>
                                           {''.join([f'<li>{item}</li>' for item in selected_items])}
-                                          </ul>''')    # Lists "selected_items" list elements
+                                          </ul>''')    # Lists 'selected_items' list elements
         self.list_widget.setSelectionMode(QtWidgets.QListWidget.SelectionMode.SingleSelection)
 
 
@@ -34,7 +34,11 @@ def add_item(self):
 def edit_item(self):
     item_to_edit = self.list_widget.currentItem()
 
-    new_item, ok = QtWidgets.QInputDialog.getText(self, "Edit item", "New item name:", QtWidgets.QLineEdit.EchoMode.Normal, item_to_edit.text())
+    new_item, ok = QtWidgets.QInputDialog.getText(self,
+                                                  'Edit item',
+                                                  'New item name:',
+                                                  QtWidgets.QLineEdit.EchoMode.Normal,
+                                                  item_to_edit.text())
     if new_item and ok:
         item_to_edit.setText(new_item)
 
