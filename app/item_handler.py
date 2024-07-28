93693ab491fbadd48553f4a2cd6c4edb8c677939
ygz213 <yigitgurbuz0630@outlook.com>
2024-07-28 19:43:15 +0300

    Cleaned up

--- app/item_handler.py
+++ app/item_handler.py
@@ -4,39 +4,39 @@ from PyQt6 import QtWidgets
 
 
 
-def select_item(self):
-    item_indices = list(list(range(0, self.list_widget.count())))
+def select_item(window):
+    item_indices = list(list(range(0, window.list_widget.count())))
     selected_items = []
 
-    self.list_widget.clearSelection()
+    window.list_widget.clearSelection()
 
-    if self.list_widget.count() > 0:
-        self.list_widget.setSelectionMode(QtWidgets.QListWidget.SelectionMode.MultiSelection)
-        for item in range(int(self.combo_box.currentText())):    # With this for loop, the program selects as many random items as user wants
+    if window.list_widget.count() > 0:
+        window.list_widget.setSelectionMode(QtWidgets.QListWidget.SelectionMode.MultiSelection)
+        for item in range(int(window.combo_box.currentText())):    # With this for loop, the program selects as many random items as user wants
             selected_item = choice(item_indices)    # Selects random item
-            selected_items.append(self.list_widget.item(selected_item).text())    # Adds random item to the 'selected_items' list
-            self.list_widget.item(selected_item).setSelected(True)    # Shows random item in 'self.list_widget'
+            selected_items.append(window.list_widget.item(selected_item).text())    # Adds random item to the 'selected_items' list
+            window.list_widget.item(selected_item).setSelected(True)    # Shows random item in 'self.list_widget'
             item_indices.remove(selected_item)    # Deletes random item from 'item_indices' so it can't be selected again
-        self.list_widget.setSelectionMode(QtWidgets.QListWidget.SelectionMode.SingleSelection)
+        window.list_widget.setSelectionMode(QtWidgets.QListWidget.SelectionMode.SingleSelection)
 
-        QtWidgets.QMessageBox.information(self, 'Selected item(s)', f'''
+        QtWidgets.QMessageBox.information(window, 'Selected item(s)', f'''
                                           <ul>
                                           {''.join([f'<li>{item}</li>' for item in selected_items])}
                                           </ul>''')    # Lists 'selected_items' list elements
 
 
-def add_item(self):
-    item, ok = QtWidgets.QInputDialog.getText(self, 'Add item', 'Item name:')
+def add_item(window):
+    item, ok = QtWidgets.QInputDialog.getText(window, 'Add item', 'Item name:')
     if item and ok:
-        self.list_widget.addItem(item)
-        self.combo_box.clear()
-        self.combo_box.addItems([str(item) for item in list(range(1, self.list_widget.count()))])
+        window.list_widget.addItem(item)
+        window.combo_box.clear()
+        window.combo_box.addItems([str(item) for item in list(range(1, window.list_widget.count()))])
 
 
-def edit_item(self):
-    item_to_edit = self.list_widget.currentItem()
+def edit_item(window):
+    item_to_edit = window.list_widget.currentItem()
 
-    new_item, ok = QtWidgets.QInputDialog.getText(self,
+    new_item, ok = QtWidgets.QInputDialog.getText(window,
                                                   'Edit item',
                                                   'New item name:',
                                                   QtWidgets.QLineEdit.EchoMode.Normal,
@@ -45,11 +45,11 @@ def edit_item(self):
         item_to_edit.setText(new_item)
 
 
-def delete_item(self):
-    item_to_delete = self.list_widget.currentItem()
-    are_you_sure = QtWidgets.QMessageBox.question(self, 'Delete item', f'Are you sure you want to delete "<b>{item_to_delete.text()}</b>"?')
+def delete_item(window):
+    item_to_delete = window.list_widget.currentItem()
+    are_you_sure = QtWidgets.QMessageBox.question(window, 'Delete item', f'Are you sure you want to delete "<b>{item_to_delete.text()}</b>"?')
 
     if are_you_sure == QtWidgets.QMessageBox.StandardButton.Yes:
-        self.list_widget.takeItem(self.list_widget.row(item_to_delete))
-        self.combo_box.clear()
-        self.combo_box.addItems([str(item) for item in list(range(1, self.list_widget.count()))])
+        window.list_widget.takeItem(window.list_widget.row(item_to_delete))
+        window.combo_box.clear()
+        window.combo_box.addItems([str(item) for item in list(range(1, window.list_widget.count()))])
--- app/list_handler.py
+++ app/list_handler.py
@@ -3,23 +3,23 @@ from PyQt6 import QtWidgets
 
 
 
-def save_list(self):
-    list_fname = QtWidgets.QFileDialog.getSaveFileName(self, 'Save list', '', '.selector File (.selector)')
+def save_list(window):
+    list_fname = QtWidgets.QFileDialog.getSaveFileName(window, 'Save list', '', 'Selector File (*.selector)')
     list_file = open(f'{list_fname[0]}.selector', 'w')
 
-    for item in [self.list_widget.item(x).text() for x in range(self.list_widget.count())]:
+    for item in [window.list_widget.item(x).text() for x in range(window.list_widget.count())]:
         list_file.write(f'{item}\n')
 
-    QtWidgets.QMessageBox.information(self, 'INFO', 'List has been saved.')
+    QtWidgets.QMessageBox.information(window, 'INFO', 'List has been saved.')
     list_file.close()
 
 
-def load_list(self):
-    list_fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Load list', '', 'Selector File (*.selector)')
+def load_list(window):
+    list_fname = QtWidgets.QFileDialog.getOpenFileName(window, 'Load list', '', 'Selector File (*.selector)')
     list_file = open(list_fname[0])
 
-    self.list_widget.clear()
+    window.list_widget.clear()
     for item in list_file.read().splitlines():
-        self.list_widget.addItem(item)
-        self.combo_box.clear()
-        self.combo_box.addItems([str(item) for item in list(range(1, self.list_widget.count()))])
+        window.list_widget.addItem(item)
+        window.combo_box.clear()
+        window.combo_box.addItems([str(item) for item in list(range(1, window.list_widget.count()))])
